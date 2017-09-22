969dd28679865c707b0b127e43f54c5530078bf1
rainwang <rainwang@hinabian.com>
2017-09-22 16:58:29 +0800

    笔记补充

---
+++ JSNativeExample/JSNativeDemo1/JSNativeDemo1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSNativeDemo1
+//
+//  Created by hnbwyh on 2017/9/22.
+//  Copyright © 2017年 hainbwyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
